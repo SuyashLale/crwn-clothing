a6ce7cd3c5f4e9cd6f4b253d4fac2925efb1e734
Suyash Lale <suyashlale@Suyashs-MacBook-Pro.local>
2021-05-20 00:16:21 +0530

    Added the Shop Page along with Collection-Preview and Collection-Item components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,8 +1,8 @@
 import {React} from 'react';
 
 import './homepage.styles.scss';
-import '../components/directory/directory.component.jsx';
-import Directory from '../components/directory/directory.component';
+import '../../components/directory/directory.component.jsx';
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => {
     return (
