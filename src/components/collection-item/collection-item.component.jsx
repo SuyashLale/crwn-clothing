a15cfae3c0727ad70c03d6b01996569160ae34ac
Suyash Lale <suyashlale@Suyashs-MacBook-Pro.local>
2021-05-23 00:22:02 +0530

    updated cart reducer to enable adding items to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
+import CustomButton from "../custom-button/custom-button.component";
+import { addItemToCart } from "../../redux/cart/cart.actions";
+
+const CollectionItem = ({ item, addItemToCart }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
         <span className="name">{name}</span>
         <span className="price">${price}</span>
+      </div>
+      <CustomButton inverted="true" onClick={() => addItemToCart(item)}>
+        ADD TO CART
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItemToCart: (item) => dispatch(addItemToCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCartHelper = (cartItems, newItem) => {
+  //Check if the newItem that is being added already exists in the cart
+  // based on the Item ID
+  const itemExists = cartItems.find((cartItem) => cartItem.id === newItem.id);
+
+  // If the itemExists is not undefined, then it means that its not a new Item,return a new array of cartItems so that react can re-render.
+  // If undefined, meaning its a new item, add it with qty = 1
+  if (itemExists) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === newItem.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  } else {
+    return [...cartItems, { ...newItem, quantity: 1 }];
+  }
+};
